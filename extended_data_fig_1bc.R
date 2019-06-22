97e06e4885b24ce6b019263eb36d4b634581e228
Gabriele Lillacci <gabriele.lillacci@gmail.com>
2019-06-22 20:52:51 +0100

    Update script for extended data figs 1bc.

--- extended_data_fig_1bc.R
+++ extended_data_fig_1bc.R
@@ -1,9 +1,10 @@
-# Script for the evalutation of relative steady-state error in API
-# NEGATIVE DISTURBANCE CASE
+# A universal biomolecular integral feedback controller for robust perfect adaptation
+# Stephanie K. Aoki, Gabriele Lillacci, Ankit Gupta, Armin Baumschlager, David Schweingruber & Mustafa Khammash
+
+# Simulations presented in Extended Data Figures 1b and 1c
 
 # Load packages
 library (lattice)
-library(animation)
 
 # Clean up & initialize
 rm (list = ls ())
@@ -19,9 +20,12 @@ k3 = 0.05
 N = c (100, 100, 100)
 
 # Create grid of log-spaced values for variable parameters
-p1seq = 10^(seq (log10 (1e-0), log10 (1e4), length.out = N[1]))
-p2seq = 10^(seq (log10 (1e-2), log10 (1e2), length.out = N[2]))
-p3seq = 10^(seq (log10 (1e-2), log10 (1e2), length.out = N[3]))
+mu_seq = 10^(seq (log10 (1e-0), log10 (1e4), length.out = N[1]))
+theta_seq = 10^(seq (log10 (1e-2), log10 (1e2), length.out = N[2]))
+k_seq = 10^(seq (log10 (1e-2), log10 (1e2), length.out = N[3]))
+
+## The following section will loop through the 3-dimensional parameter grid for mu, theta and k
+## and evaluate the steady-state error at each point
 
 # Preallocate array to store the essr and the ssy0
 essr = array (0, c(N, 3))
@@ -34,18 +38,18 @@ for (i in seq (1, N[1]))
 		for (k in seq (1, N[3]))
 		{
 			# Set sequence values to current parameter values
-			k1 = p1seq[i]
-			k2 = p2seq[j]
-			k4 = p3seq[k]
+			k1 = mu_seq[i]
+			k2 = theta_seq[j]
+			k4 = k_seq[k]
 			
 			for (z in seq (1,3))
 			{
 				# Set d to the current 
 				d = da[z]
 				
-				# Analytical steady state from Mathematica
+				# Analytical steady state WITH disturbance
 				ssyw = (2*d*k1*k4)/((d+w+k5)*(d^2-k1*k3 + sqrt((d^2 + k1*k3)^2 + (4*d*k1*k2*k3*k4)/(d + w + k5))))
-				# Analytical steady state from Mathematica without disturbance
+				# Analytical steady state WIHTOUT disturbance
 				ssynw = (2*d*k1*k4)/((d+k5)*(d^2-k1*k3+sqrt(d^4+(k1^2*k3^2)+2*d*k1*k3*(d+(2*k2*k4)/(d+k5)))))
 				
 				# Calculate and store the essr
@@ -57,37 +61,41 @@ for (i in seq (1, N[1]))
 	}
 }
 
-# Display the percentage of grid points with rel error less than 5%
+# Display the percentage of grid points with error less than 5%
 cat ('\n\nPercentage of grid points with less than 5% relative error:\n')
 cat (sum(essr<0.05)/length(essr))
 cat ('\n')
 
 
-# Function to create animation
+## Plotting function. This function will generate a 2-d plot showing a slice of the 3D parameter grid,
+## i.e. a 2D plot for a fixed value of k. The argument s is the index of k_seq containing the value of k to plot.
+
 anifun = function (s) 
 {
 	# Init graphic device
 	quartz (width = 7, height = 5)
 	par (mar = c (4, 4, 0, 0) + 0.2)
 	# Set axes labels
-	xl = expression ('k'[1] ~ 'log nM'^-1 ~ 'min'^-1)
-	yl = expression ('k'[2] ~ 'log min'^-1)
+	xl = expression ('mu' ~ 'log nM'^-1 ~ 'min'^-1)
+	yl = expression ('theta' ~ 'log min'^-1)
 	# Call empty plot
-	image (log10(p1seq), log10(p2seq), essr[,,s,1], col = 'white', xlab = xl, ylab = yl, main = NA)
+	image (log10(mu_seq), log10(theta_seq), essr[,,s,1], col = 'white', xlab = xl, ylab = yl, main = NA)
 	# Add region of essr < 0.05 for mammalian cells
-	.filled.contour(log10(p1seq), log10(p2seq), essr[,,s,3], levels = c(0, 0.05), col = c('khaki2', 'white'))
+	.filled.contour(log10(mu_seq), log10(theta_seq), essr[,,s,3], levels = c(0, 0.05), col = c('khaki2', 'white'))
 	# Add region of essr < 0.05 for yeast
-	.filled.contour(log10(p1seq), log10(p2seq), essr[,,s,2], levels = c(0, 0.05), col = c('lightblue', 'white'))
+	.filled.contour(log10(mu_seq), log10(theta_seq), essr[,,s,2], levels = c(0, 0.05), col = c('lightblue', 'white'))
 	# Add region of essr < 0.05 for bacteria
-	.filled.contour(log10(p1seq), log10(p2seq), essr[,,s,1], levels = c(0, 0.05), col = c('lightgreen', 'white'))
+	.filled.contour(log10(mu_seq), log10(theta_seq), essr[,,s,1], levels = c(0, 0.05), col = c('lightgreen', 'white'))
 	# Calculate levels for contours of ssy0
 	levs = 10^(seq( log10 (min(ssy0[,,s,1])), log10(max(ssy0[,,s,1])), length.out = 20))
 	levs = signif (levs, 3)
 	# Add contour plots of ssy0
-	contour(log10(p1seq), log10(p2seq), ssy0[,,s,1], levels = levs, labcex = 1, add=T)
+	contour(log10(mu_seq), log10(theta_seq), ssy0[,,s,1], levels = levs, labcex = 1, add=T)
 }
 
 
-# Create the animation
-# setwd ('~/Desktop')
-# saveGIF (for (s in 1:N[3]) anifun(s), movie.name = 'API_sser_nd.gif') 
+## Plot the figures in the paper
+## This generates extended data fig. 1b
+anifun(15)
+## This generates extended data fig. 1c
+anifun(65)
